f151d106d272909228b27f5e9d0fbe369350b181
River Phillips <riverphillips1@gmail.com>
2018-11-30 19:22:40 +0000

    Normalize the data

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
 
-data_source = 'alpha_vantage'
+data_source = 'kaggle'
 
 if data_source == 'alpha_vantage':
     api_key = ''
@@ -44,3 +44,45 @@ else:
     df = pd.read_csv(os.path.join('Stocks', 'hpq.us.txt'), delimiter=',', usecols=[
                      'Date', 'Open', 'High', 'Low', 'Close'])
     print('Loaded data from the Kaggle repository')
+
+df = df.sort_values('Date')
+
+# plt.figure(figsize=(18, 9))
+# plt.plot(range(df.shape[0]), (df['Low']+df['High'])/2.0)
+# plt.xticks(range(0, df.shape[0], 500), df['Date'].loc[::500], rotation=45)
+# plt.xlabel('Date', fontsize=18)
+# plt.ylabel('Mid Price', fontsize=18)
+# plt.show()
+
+high_prices = df.loc[:, 'High'].as_matrix()
+low_prices = df.loc[:, 'Low'].as_matrix()
+mid_prices = (high_prices/low_prices)/2
+
+train_data = mid_prices[:11000]
+test_data = mid_prices[11000:]
+
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1, 1)
+test_data = test_data.reshape(-1, 1)
+
+smoothing_window_size = 2500
+for di in range(0, 10000, smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size, :])
+    train_data[di:di+smoothing_window_size,
+               :] = scaler.transform(train_data[di:di+smoothing_window_size, :])
+
+scaler.fit(train_data[di+smoothing_window_size:, :])
+train_data[di+smoothing_window_size:,
+           :] = scaler.transform(train_data[di+smoothing_window_size:, :])
+
+train_data = train_data.reshape(-1)
+
+test_data = scaler.transform(test_data).reshape(-1)
+
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+    EMA = gamma*train_data[ti] + (1-gamma)*EMA
+    train_data[ti] = EMA
+
+all_mid_data = np.concatenate([train_data, test_data], axis=0)
